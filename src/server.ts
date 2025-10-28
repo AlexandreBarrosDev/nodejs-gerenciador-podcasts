7aabf7925e4de3d4983c570c8f02a38d63d87e7c
Alexandre Barros <alexandrebarros.dev@gmail.com>
2025-10-28 12:13:14 -0300

    criado o servidor e controller de podcasts

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import http, { get } from 'http';
+
+import { getListEpisodes } from './controllers/podcasts-controller';
+
+const server = http.createServer(
+    async (req: http.IncomingMessage, res: http.ServerResponse) => {
+        if (req.method === 'GET') {
+            await getListEpisodes(req, res);
+        }
+    }
+);
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
